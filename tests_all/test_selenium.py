c33d41db6e344eb526b402c9aa38d47c495479db
Ylik <juli.01071369@gmail.com>
2024-01-30 14:21:42 +0400

    Fixed

--- tests_all/test_selenium.py
+++ tests_all/test_selenium.py
@@ -1,5 +1,5 @@
 import time
-import pytest
+import  pytest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
@@ -11,27 +11,28 @@ from selenium.webdriver.common.by import By
 def driver_chrome():
     driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
     driver.get("https://www.21vek.by")
+    yield driver
+    driver.close()
+    driver.quit()
+
+@pytest.fixture
+def handle_cookie(driver_chrome):
     try:
-        cookie_overlay = driver.find_element(By.XPATH, '//*[@id="modal-cookie"]/div')
+        cookie_overlay = driver_chrome.find_element(By.XPATH, '//*[@id="modal-cookie"]/div')
         if cookie_overlay:
             accept_button = cookie_overlay.find_element(By.XPATH,
                                                         '//*[@id="modal-cookie"]/div/div[2]/div[2]/button[2]/div')
             accept_button.click()
-    except:
-        pass
-    yield driver
-    driver.close()
-    driver.quit()
+    except Exception as e:
+        print(f"Error handling cookies: {e}")
 
 def test_product_search(driver_chrome):
     search_box=driver_chrome.find_element(By.ID,"catalogSearch")
     search_box.send_keys("Телевизоры")
     search_box.send_keys(Keys.RETURN)
     time.sleep(5)
-    current_url = driver_chrome.current_url
-    if current_url == "https://www.21vek.by/tv/":
-        driver_chrome.get("https://www.21vek.by")
-    time.sleep(5)
+    result=driver_chrome.find_element(By.XPATH,"(//*[@class='b-recipes__item__link j-category__link'])[1]")
+    assert result
 
 def test_navigate_to_shop_from_menu(driver_chrome):
     xpath = '//*[@id="header"]/div/div[4]/div/div/ul/li[3]/a'
@@ -41,18 +42,14 @@ def test_navigate_to_shop_from_menu(driver_chrome):
     assert driver_chrome.current_url == 'https://www.21vek.by/refrigerators/'
     time.sleep(5)
 
-def test_adding_in_cart(driver_chrome):
+def test_adding_in_cart(driver_chrome,handle_cookie):
     xpath = '//*[@id="content"]/div[2]/section/div/div/div/div/div/div/div[1]/div/div/div[3]/div[3]/button/div'
     add_to_card = driver_chrome.find_element(By.XPATH, xpath)
     add_to_card.click()
     time.sleep(5)
-    try:
-        notification_element = driver_chrome.find_element(By.XPATH,
-                                                          '//*[@id="header"]/div/div[3]/div/div[4]/a/span[1]/span')
-        assert notification_element.is_displayed(), "Товар не был добавлен в корзину"
-    except NoSuchElementException:
-        pytest.fail("Товар не был добавлен в корзину")
-def test_social_media_button(driver_chrome):
+    assert driver_chrome.find_element(By.XPATH,
+                                      '//*[@id="header"]/div/div[3]/div/div[4]/a/span[1]/span')
+def test_social_media_button(driver_chrome,handle_cookie):
     xpath = '//*[@id="footer-inner"]/div/div/div[3]/a[3]/div'
     instagram_button=driver_chrome.find_element(By.XPATH,xpath)
     time.sleep(5)
@@ -65,7 +62,7 @@ def find_and_click_element(driver, xpath):
     element = driver.find_element(By.XPATH, xpath)
     element.click()
 
-def test_login_password(driver_chrome):
+def test_login_password(driver_chrome,handle_cookie):
     user_button_xpath = '//*[@id="header"]/div/div[3]/div/div[3]/div/div/div/button'
     user_button_dropdown_xpath = '//*[@id="userToolsDropDown"]/div/div[1]/div[2]/button'
     find_and_click_element(driver_chrome, user_button_xpath)
